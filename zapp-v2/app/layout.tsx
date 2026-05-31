f2e6dd4040ef4e6d8179cc06fc0f472cffeb3c98
anatomyofagoalofficial-lab <anatomyofagoal.official@gmail.com>
2026-05-31 18:23:32 +0200

    Signature energy cursor: gold comet trail + sparks following the pointer (Tesla electricity as the cursor), mouse-only + reduced-motion safe

--- zapp-v2/app/layout.tsx
+++ zapp-v2/app/layout.tsx
@@ -7,6 +7,7 @@ import { Footer } from "@/components/Footer";
 import { ScrollProgress } from "@/components/ScrollProgress";
 import { TransitionProvider } from "@/components/TransitionProvider";
 import { SmoothScroll } from "@/components/SmoothScroll";
+import { EnergyCursor } from "@/components/EnergyCursor";
 
 const cormorant = Cormorant_Garamond({
   subsets: ["latin"],
@@ -68,6 +69,7 @@ export default function RootLayout({
     >
       <body className="min-h-screen bg-present-black font-sans text-present-white antialiased">
         <SmoothScroll>
+          <EnergyCursor />
           <ScrollProgress />
           <TransitionProvider>
             <Header />
